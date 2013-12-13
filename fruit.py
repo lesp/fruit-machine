9508bc7d7ef0ed5fa49514712d12680ed0c54f8c
Les Pounder <lespounder@gmail.com>
2013-12-13 22:30:38 +0000

    first commit during another ad break in Mock the Week

---
+++ fruit.py
@@ -0,0 +1,25 @@
+from random import *
+from time import *
+
+reel1 = ["cherry","lemon","bell"]
+reel2 = ["cherry","lemon","bell"]
+reel3 = ["cherry","lemon","bell"]
+
+while True:
+    if raw_input() == "p":
+        for i in range(0,3):
+            r1 = choice(reel1)
+            r2 = choice(reel2)
+            r3 = choice(reel3)
+
+            print (r1) + " " + (r2) + " " + (r3)
+            sleep(2)
+
+            if (r1) == (r2) and (r1) == (r3):
+                print ("Jackpot")
+            elif (r2) == (r1) and (r2) == (r3):
+                print ("Jackpot")
+            elif (r3) == (r1) and (r3) == (r2):
+                print ("Jackpot")
+    else:
+        print ("End of game")
