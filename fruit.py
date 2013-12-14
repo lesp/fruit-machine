4ebb5de43596f47b20429443c07f8f13cf54f2e9
Les Pounder <lespounder@gmail.com>
2013-12-14 09:54:15 +0000

    added more print statements to tidy the game up

--- fruit.py
+++ fruit.py
@@ -11,16 +11,24 @@ while True:
             r1 = choice(reel1)
             r2 = choice(reel2)
             r3 = choice(reel3)
-
+            print ("++Playing the game++")
+            print (" ")
             print (r1) + " " + (r2) + " " + (r3)
+            print (" ")
             sleep(2)
 
             if (r1) == (r2) and (r1) == (r3):
+                print ("=======")
                 print ("Jackpot")
+                print ("=======")
             elif (r2) == (r1) and (r2) == (r3):
+                print ("=======")
                 print ("Jackpot")
+                print ("=======")
             elif (r3) == (r1) and (r3) == (r2):
+                print ("=======")
                 print ("Jackpot")
+                print ("=======")
     else:
         print ("End of game")
         break
